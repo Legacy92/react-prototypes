6b73f2fd6d16624d66ede63a0831ba60f3274a2d
Legacy92 <colemancorey22@gmail.com>
2018-04-26 20:08:00 -0700

    finished functional components prototype

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,30 @@
+import React from 'react';
+import Table from './table.js';
 
+const students = [
+    {
+        name: 'Hanh Pham',
+        course: 'Intro to Psychology',
+        grade: 50
+    },
+    {
+        name: 'Mike Jones',
+        course: 'Math',
+        grade: 75
+    },
+    {
+        name: 'Tony Tran',
+        course: 'Science',
+        grade: 80
+    }
+];
+
+export default () => {
+    return (
+        <div className="container">
+            <h1>Student Grade Table</h1>
+            <Table data={students}/>
+        </div>
+    )
+};
 

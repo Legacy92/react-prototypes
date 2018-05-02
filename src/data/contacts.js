2fb90fab0dfed599d9656af6a30895e083d94d9d
Legacy92 <colemancorey22@gmail.com>
2018-05-01 20:32:30 -0700

    finished forms-inputs prototype

---
+++ src/data/contacts.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        firstName: 'Corey',
+        lastName: 'Coleman',
+        phone: '888-888-8888',
+        email: 'ohitsyou@gmail.com'
+    },
+    {
+        firstName: 'Andrew',
+        lastName: 'Harrington',
+        phone: '111-111-1111',
+        email: 'vapelife@vape.com'
+    },
+    {
+        firstName: 'Daniel',
+        lastName: 'ShitTaker',
+        phone: '222-222-2222',
+        email: 'number2Taker@shitty.com'
+    },
+    {
+        firstName: 'Harrison',
+        lastName: 'Scumbag',
+        phone: '444-444-4444',
+        email: 'scumMania@aol.com'
+    }
+];

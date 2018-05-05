6395f393d56b5baae1878d095ce04d458504ed88
Legacy92 <colemancorey22@gmail.com>
2018-05-05 13:35:23 -0700

    finished styles-images react prototype

---
+++ src/components/image_data.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import Daniel from './images/Daniel.jpg';
+import Group from './images/Group.jpg';
+import Hanh from './images/Hanh.jpg';
+import Harrison from './images/Harrison.png';
+import KeithWorm from './images/KeithWorm.png';
+import ME from './images/ME.png';
+import Scrum from './images/Scrum.jpg';
+import Sensei from './images/Sensei.jpg';
+
+export default [
+    {
+        src: Daniel,
+        style: {
+            left: '5%',
+            transform: 'rotate(-10deg)'
+        }
+    },
+    {
+        src: Group,
+        style: {
+            left: '40%',
+            transform: 'rotate(6deg)'
+        }
+    },
+    {
+        src: Hanh,
+        style: {
+            right: '15%',
+            transform: 'rotate(-9deg)'
+        }
+    },
+    {
+        src: Harrison,
+        style: {
+            left: '8%',
+            top: '28%',
+            transform: 'rotate(8deg)'
+        }
+    },
+    {
+        src: KeithWorm,
+        style: {
+            left: '37%',
+            top: '25%',
+            transform: 'rotate(-12deg)'
+        }
+    },
+    {
+        src: ME,
+        style: {
+            right: '12%',
+            top: '26%',
+            transform: 'rotate(10deg)'
+        }
+    },
+    {
+        src: Scrum,
+        style: {
+            left: '25%',
+            top: '48%',
+            transform: 'rotate(-5deg)'
+        }
+    },
+    {
+        src: Sensei,
+        style: {
+            right: '25%',
+            top: '52%',
+            transform: 'rotate(16deg)'
+        }
+    }
+];
+
+
--- src/components/scrapbook.js
+++ src/components/scrapbook.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import Daniel from "./images/Daniel.jpg"
 import './scrapbook.css';
+import imageData from './image_data';
+import ScrapbookImage from './scrapbook_images';
 
 export default props => {
+    const images = imageData.map((img, index) => {
+        return <ScrapbookImage key={index} about={img} />
+    });
     return (
         <div className="scrapbook-container">
-            <img src={Daniel}/>
+        {images}
         </div>
     )
 }

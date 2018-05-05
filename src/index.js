ff51bc4cf52a41d759afff5bd413730e4c8d0382
Legacy92 <colemancorey22@gmail.com>
2018-05-05 16:07:34 -0700

    MVP for react-axios prototype

--- src/index.js
+++ src/index.js
@@ -1,32 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-
-let user = {
-    name: 'Corey',
-    luckyNumber: luckyNumber()
-}
+import App from './components/app.js';
 
 ReactDOM.render(
-greeting(user),
-document.getElementById('root')
+    <App/>,
+    document.getElementById('root')
 );
-
-function luckyNumber() {
-   return Math.floor(Math.random() * 1000) + 1;
-}
-
-function greeting(user) {
-   let name = user.name;
-    let luckyNumber = user.luckyNumber;
-    return (
-        <div className="container">
-            <h1>Welcome {name}</h1>
-            <h2 className="text-muted">Your lucky number is {luckyNumber}</h2>
-        </div>
-    )
-}
-
 
 
 

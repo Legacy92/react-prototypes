0ba5b7581791799efcbe5edf6f48fa8c2a16f5a0
Legacy92 <colemancorey22@gmail.com>
2018-06-10 22:42:42 -0700

    added reset functionality to input forms

--- src/components/contact_form.js
+++ src/components/contact_form.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Field from './field.js';
 
 class ContactForm extends Component {
-    constructor(props) {
+    constructor(props){
         super(props);
 
         this.state = {
@@ -13,35 +13,56 @@ class ContactForm extends Component {
                 email: ''
             }
         }
+
         this.handleInputChange = this.handleInputChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.reset = this.reset.bind(this);
     }
 
-    handleInputChange(event) {
+    handleInputChange(event){
         const { value, name } = event.target;
+
         const { form } = this.state;
+
+        form[name] = value;
+
         this.setState({
-            form: {
-                [name]: value,
-            }
-        })
+            form: {...form}
+        });
     }
 
-    handleSubmit(event) {
+    handleSubmit(event){
         event.preventDefault();
         console.log('handleSubmit called, form values are:', this.state.form);
+
         this.props.add(this.state.form);
+
+        this.reset();
     }
 
-    render() {
+    reset(){
+        this.setState({
+            form: {
+                firstName: '',
+                lastName: '',
+                phone: '',
+                email: ''
+            }
+        });
+    }
+    
+    render(){
         const { firstName, lastName, phone, email } = this.state.form;
+
         return (
             <form onSubmit={this.handleSubmit}>
-                <Field name="firstName" label="First Name" type="text" value={firstName} onChange={this.handleInputChange} />
-                <Field name='lastName' label='Last Name' type='text' value={lastName} onChange={this.handleInputChange} />
-                <Field name="phone" label="Phone Number" type="tel" value={phone} onChange={this.handleInputChange} />
-                <Field name="email" label="Email" type="text" value={email} onChange={this.handleInputChange} />
+                <Field name="firstName" label="First Name" type="text" value={firstName} onChange={this.handleInputChange}/>
+                <Field name="lastName" label="Last Name" type="text" value={lastName} onChange={this.handleInputChange}/>
+                <Field name="phone" label="Phone Number" type="tel" value={phone} onChange={this.handleInputChange}/>
+                <Field name="email" label="Email" type="email" value={email} onChange={this.handleInputChange}/>
+
                 <button>Add Contact</button>
+                <button type="button" onClick={this.reset}>Clear Form</button>
             </form>
         )
     }
